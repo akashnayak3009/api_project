08ce629e54c2169b2b3684edd11c0efe48965d03
Akash kumar Nayak <akashnayak3009@gmail.com>
2023-10-23 01:20:24 +0530

    db

---
+++ mongodb_api/index.js
@@ -0,0 +1,80 @@
+// managing Databases and collections
+// show dbs;
+// use <database-name>;
+// db.dropDatabase();
+// show collections;
+//db.createCollection('<collection-name>');
+
+//db.<collection-name>.drop();
+// Inserting Documents in mongodb
+//db.collection-name.insertOne({
+// field1:value1,
+//field2:value2,
+//})
+//db.collection-name.insertMany({
+//field1:value1,
+//field2:value2,
+//},
+//{
+// field1:value1,
+// field2:value2
+//})
+//db.collection-name.find();
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
